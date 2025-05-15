94a00754d93813740e4ff45d82c386743c97a0d5
k-midz-nmadj <k_midz_nmadj@hotmail.com>
2025-05-15 20:07:26 +0900

    Add files via upload

---
+++ IocpServer.h
@@ -0,0 +1,294 @@
+/////////////////////////////////////////////////////////////////////////////
+// IocpServer.h - IOCPクラスのソケット拡張
+//
+// [継承関係]
+//             CIocpOverlapped <= 
+// CSocketBase <= CSocketAsync <= CIocpSocket
+// CAPCThread  <= CIocpThread  <= CTLHeap <= CSocketFactory  <=
+//                                           CIocpThreadPool <= CIocpServer
+// [包含関係]
+// CIocpThreadPool<CSocketFactory> *- CSocketFactory +- CTLHList<CIocpSocket> *- CIocpSocket
+// 
+// [参照関係]
+// CIocpServer <- CSocketFactory <- CIocpSocket
+// 
+#pragma once
+
+#include "iocp.h"
+#include "tlheap.h"
+#include "sockbase.h"
+
+class CSocketFactory;	// ソケット作成クラス宣言
+
+typedef CIocpOverlapped<CSocketFactory> CSocketOverlapped;	// クライアントクラス
+typedef CIocpThread<CSocketFactory>     CSocketThread;		// スレッドクラス
+typedef CIocpThreadPool<CSocketFactory> CIocpServerBase;	// サーバクラス
+
+// 非同期(重複IO)ソケットクラス
+class CSocketAsync : public CSocketBase
+{
+public:
+	BOOL Socket(int af = AF_INET, int type = SOCK_STREAM, int protocol = IPPROTO_TCP);	// 重複IOソケット作成
+	SOCKET Attach(CSocketAsync& sock);	// 外部ソケットのハンドル割付け
+};
+
+// IOCP用ソケットクラス
+class CIocpSocket : public CSocketOverlapped, public CSocketAsync
+{
+	friend class CSocketFactory;
+protected:
+	enum WSA_IOEVENT	// ソケットイベントID
+	{
+		IOInit = 0,	// 初期状態
+		IOSuspend,	// 一時停止
+		IOAccept,	// 接続受入待ち
+		IOConnect,	// 接続完了待ち
+		IOReceive,	// 受信完了待ち
+		IOSend,		// 送信完了待ち
+	} m_ioState;
+	
+	LPWSABUF m_pWsaBuf;	// 送受信保留データ
+	CSocketFactory* m_pSockFct;	// Socket作成I/F
+	
+	CIocpSocket();
+	~CIocpSocket();
+	
+	BOOL OnCompletionIO(CSocketFactory* pFct);	// IO完了イベント
+	
+	virtual BOOL OnAccept(const CSockAddrIn* pAddrRemote = NULL);	// 接続受入イベント
+	virtual BOOL OnConnect();	// 接続完了イベント
+	virtual BOOL OnTimeout();	// タイムアウトイベント
+	
+	virtual int OnRead(int iIoSize);	// 受信完了イベント
+	virtual int OnWrite(int iIoSize);	// 送信完了イベント
+
+public:
+	operator HANDLE();	// IOCP用のハンドル取得
+	
+	virtual BOOL Start();	// 受信開始
+	BOOL Stop();			// 一時停止
+	int IsPending();		// イベント待機中判定
+	
+	int Write(LPCVOID pBuff, int iSendSize);	// 非同期送信
+#if(_WIN32_WINNT >= 0x0501)
+	BOOL Connect(const SOCKADDR* ai_addr, int ai_addrlen);	// クライアントの接続開始
+	BOOL Connect(const CSockAddrIn& addrRemote)
+	{
+		return Connect(addrRemote, sizeof(addrRemote));
+	}
+#endif
+};
+
+// IOCP用ソケット作成クラス
+class CSocketFactory : public CTLHeap<CSocketThread>
+{
+protected:
+	typedef CTLHList<CIocpSocket, CTLHeap> CSocketList;
+	CSocketList m_listSocket;	// ソケットリストコンテナ
+	
+public:
+	CSocketFactory(DWORD dwHeapOpt = HEAP_NO_SERIALIZE);
+	~CSocketFactory();
+	
+	BOOL FindSocket(CIocpSocket* pSocket);	// ソケット検索
+	BOOL DeleteSocket(CIocpSocket* pSocket);// ソケット削除
+	
+	VOID OnCancelIO(CSocketOverlapped* pIO);// IOキャンセルイベント
+	VOID OnTimeout(DWORD dwCurrentTime);	// タイムアウトイベント
+	
+	// ソケット作成(内部コンテナへの追加)
+	template <class TYPE>	// TYPE: CIocpSocket派生クラス
+	TYPE* CreateSocket(const CSockAddrIn* pAddr = NULL)
+	{
+		if (!m_pIocp)
+			return NULL;
+		
+		ISBASE_TYPE(TYPE, CIocpSocket);
+		TYPE* pSocket = m_listSocket.AddItem<TYPE>(this);	// ソケットを作成しリストに追加
+		
+		if (pSocket && 
+			(!pSocket->Socket() || !pSocket->Bind(pAddr ? *pAddr : CSockAddrIn()) ||
+			 !m_pIocp->CreateIocp(pSocket)))	// 作成したソケットをIOCPに関連付ける
+		{
+			CSocketList::Delete(pSocket);	// 失敗時はリストから削除
+			pSocket = NULL;
+		}
+		return pSocket;
+	}
+	
+	// サーバソケット追加(nPort: ポート番号)
+	template <class TYPE>	// TYPE: CIocpSocket派生クラス
+	CIocpSocket* AddListener(USHORT nPort, int nConnections = SOMAXCONN)
+	{
+		class CListenSocket : public CIocpSocket
+		{
+			CSocketAsync sckAcpt;	// 接続受入れソケット
+			CSockAddrIn addrRemote;	// 接続先アドレス情報
+			
+			BOOL OnAccept(const CSockAddrIn* pAddrRemote)	// 接続受入れ完了イベント
+			{
+				// 受入れたソケットにローカルのアドレス情報を反映
+				if (!sckAcpt.SetSockOpt(SO_UPDATE_ACCEPT_CONTEXT,
+						reinterpret_cast<char*>(&m_hSocket), sizeof(m_hSocket)))
+					return FALSE;	// 戻り値=FALSEで自サーバソケットを削除
+				
+				// ソケットを作成しリストに追加
+				ISBASE_TYPE(TYPE, CIocpSocket);
+				CIocpSocket* pAccept = m_pSockFct->m_listSocket.AddItem<TYPE>(m_pSockFct);
+				if (!pAccept)
+					return FALSE;
+				
+				pAccept->Attach(sckAcpt);	// 作成したソケットに受入れたソケットのハンドルを割当
+				pAccept->m_pSockFct = m_pSockFct;
+				if (!pAccept->OnAccept(&addrRemote))	// 受入れイベントを発行
+					Stop();	// 戻り値=FALSEでAccept停止
+				
+				// ソケット有効時は、IOCPに関連付けて受信開始
+				if (!pAccept->IsValid() || !m_pSockFct->m_pIocp->CreateIocp(pAccept) ||
+					!pAccept->IsPending() && !pAccept->Start())
+					CSocketList::Delete(pAccept);
+				
+				return TRUE;
+			}
+			BOOL OnTimeout()
+			{
+				return TRUE;
+			}
+		public:
+			BOOL Start()	// 接続受入れ(Accept)開始
+			{
+				if (IsPending() >= 0 && sckAcpt.Socket())	// ソケット作成成功
+				{
+					if (AcceptEx(m_hSocket, sckAcpt, 
+							&addrRemote, 0, 0, sizeof(addrRemote), 
+							NULL, &m_ovl) ||
+							WSAGetLastError() == WSA_IO_PENDING)
+					{
+						m_ioState = IOAccept;	// 非同期Accept成功でイベント状態更新
+						return TRUE;
+					}
+					sckAcpt.Close();	// Accept失敗時はソケット解放
+				}
+				return FALSE;
+			}
+		} *pListener = CreateSocket<CListenSocket>(&CSockAddrIn(nPort));	// サーバソケット作成
+		
+		// ソケット作成に成功したら接続の受け入れ開始
+		if (pListener && (!pListener->Listen(nConnections) || !pListener->Start()))
+		{
+			CSocketList::Delete(pListener);	// 失敗時は削除
+			pListener = NULL;
+		}
+		return pListener;
+	}
+	
+	// クライアントソケット追加(addrRemote: IPアドレス)
+	template <class TYPE>	// TYPE: CIocpSocket派生クラス
+	TYPE* AddConnection(const CSockAddrIn& addrRemote)
+	{
+		TYPE* pConnect = CreateSocket<TYPE>();	// クライアントソケット作成
+		
+		// ソケット作成に成功したら接続開始
+		if (pConnect && !pConnect->Connect(addrRemote))
+		{
+			CSocketList::Delete(pConnect);	// 失敗時は削除
+			pConnect = NULL;
+		}
+		return pConnect;
+	}
+	
+#if (_WIN32_WINNT >= 0x0600)
+	// クライアントソケット追加(pName:ドメイン名、pServiceName:サービス名)
+	template <class TYPE>	// TYPE: CIocpSocket派生クラス
+	TYPE* AddConnection(LPCTSTR pName, LPCTSTR pServiceName)
+	{
+		struct CAddrInfoQuery : public TYPE	// アドレス情報(ADDRINFOEX)取得用の派生クラス
+		{
+			PADDRINFOEX pResult;
+			
+			static VOID WINAPI QueryComplete(DWORD dwError, DWORD dwBytes, LPWSAOVERLAPPED lpOverlapped)
+			{
+				PADDRINFOEX* ppResult = static_cast<PADDRINFOEX*>(lpOverlapped->Pointer);	// アドレス情報
+				TYPE* pThis = reinterpret_cast<TYPE*>(ppResult) - 1;	// 作成したソケット
+				
+				// 名前解決に成功したら接続開始
+				::ZeroMemory(&pThis->m_ovl, sizeof(pThis->m_ovl));
+				if (dwError != ERROR_SUCCESS || 
+					!pThis->Connect((*ppResult)->ai_addr, (int)(*ppResult)->ai_addrlen))
+					CSocketList::Delete(pThis);	// 失敗時は削除
+				
+				FreeAddrInfoEx(*ppResult);	// 取得したアドレス情報(ADDRINFOEX)を解放
+			}
+		} *pConnect = CreateSocket<CAddrInfoQuery>();	// クライアントソケット作成
+		HANDLE hCancel;
+		
+		// ドメイン名、サービス名による名前解決を非同期(重複IO)で実行
+		if (pConnect && 
+			 GetAddrInfoEx(pName, pServiceName, NS_DNS, NULL, NULL, &pConnect->pResult,
+						NULL, &pConnect->m_ovl, pConnect->QueryComplete, &hCancel) != WSA_IO_PENDING)
+		{
+			CSocketList::Delete(pConnect);	// 失敗時は削除
+			pConnect = NULL;
+		}
+		return pConnect;
+	}
+#endif
+};
+
+// IOCP対応サーバークラス
+// (解放時にスレッド終了を優先するため、継承順はCIocpThreadPoolが後)
+class CIocpServer : public CSocketFactory, public CIocpServerBase
+{
+protected:
+	VOID OnPostEvent(LPOVERLAPPED_ENTRY lpCPEntry);	// ユーザイベント
+	
+public:
+	CIocpServer(DWORD nThreadCnt = 0);
+	
+	// CTLHeap::ApcMsgSend用パラメータクラス
+	// (スレッドプール側で、非同期に永続ポートの追加と削除を実行)
+	template <class TYPE = VOID>
+	class AddPort	// ポート追加: src->ApcMsgSend(dst, CIocpServer::AddPort<TYPE>(nPort));
+	{
+		USHORT m_nPort;		// ポート番号
+		int m_nConnections;	// 最大接続数(省略可)
+	public:
+		AddPort(USHORT nPort, int nConnections = SOMAXCONN)
+		{
+			m_nPort = nPort;
+			m_nConnections = nConnections;
+		}
+		void operator ()(CIocpServer* pServer)
+		{
+			pServer->AddListener<TYPE>(m_nPort, m_nConnections);
+		}
+	};
+	class DelPort	// ポート削除: src->ApcMsgSend(dst, CIocpServer::DelPort(nPort));
+	{
+		USHORT m_nPort;	// ポート番号(省略時(0)は全削除)
+	public:
+		DelPort(USHORT nPort = 0);
+		void operator ()(CIocpServer* pServer);
+	};
+	
+	// サーバ起動(TYPE: CIocpSocket派生クラス, SYNC: 同期/非同期)
+	template <class TYPE, bool SYNC = TRUE>
+	BOOL Listen(USHORT nPort, int nConnections = SOMAXCONN, 
+				DWORD nThreadCnt = 0, DWORD nKeepAlive = MIN_KEEPALIVE)
+	{
+		if (IsRunning() || !CreateIocp(NULL, nThreadCnt))	// IOCP作成
+			return FALSE;
+		
+		// サーバソケット作成
+		CIocpSocket* pListener = AddListener<TYPE>(nPort, nConnections);
+		if (!pListener)
+			return FALSE;
+		
+		// スレッドプール実行(実行直後、ユーザイベントによってスレッド割当)
+		BOOL bStart = (PostEvent(SYNC) && Start(nKeepAlive, SYNC));
+		if (SYNC || !bStart)
+			DeleteSocket(pListener);
+		
+		return bStart;
+	}
+};
---
+++ sockbase.h
@@ -0,0 +1,224 @@
+/////////////////////////////////////////////////////////////////////////////
+// sockbase.h - ソケット関連のラッパークラス群
+//
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+//#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+
+// WinSockの初期化と解放
+struct CWSAStartup
+{
+	CWSAStartup(WSADATA* pWSAData = NULL)
+	{
+		WSADATA WSAData;
+		if (!pWSAData)	// パラメータ省略時
+			pWSAData = &WSAData;
+		
+		WSAStartup(WINSOCK_VERSION, pWSAData);	// 最新バージョンで初期化
+	}
+	~CWSAStartup()
+	{
+		WSACleanup();
+	}
+};
+#define WSA_STARTUP CWSAStartup __wsu__;	// WinSock初期化マクロ
+
+// ソケットのアドレス情報(IPv4,IPv6共通)
+struct CSockAddrIn
+{
+	ADDRESS_FAMILY sin_family;
+	USHORT sin_port;
+	CHAR __si_pad[28];
+	
+	// ポートとIPv4アドレスによる初期化
+	CSockAddrIn(USHORT port = 0, ADDRESS_FAMILY family = AF_INET)
+	{
+		sin_family = family;
+		sin_port = htons(port);
+		
+		::ZeroMemory(__si_pad, sizeof(__si_pad));
+	}
+	CSockAddrIn(const char* addr_v4, USHORT port)
+	{
+		sin_family = AF_INET;
+		sin_port = htons(port);
+		
+		((SOCKADDR_IN*)*this)->sin_addr.s_addr = inet_addr(addr_v4);
+	}
+	
+	// SOCKADDR構造体による初期化
+	CSockAddrIn(const SOCKADDR_IN& addr)
+	{
+		::CopyMemory(this, &addr, sizeof(addr));
+	}
+	CSockAddrIn(const SOCKADDR_IN6& addr)
+	{
+		::CopyMemory(this, &addr, sizeof(addr));
+	}
+	
+	// キャスト演算子によるポートとアドレスの取得
+	operator USHORT()
+	{
+		return ntohs(sin_port);
+	}
+	operator SOCKADDR*()
+	{
+		return reinterpret_cast<SOCKADDR*>(this);
+	}
+	operator const SOCKADDR*() const
+	{
+		return reinterpret_cast<const SOCKADDR*>(this);
+	}
+	
+	operator SOCKADDR_IN*()
+	{
+		return (sin_family == AF_INET ? reinterpret_cast<SOCKADDR_IN*>(this) : 0);
+	}
+	operator const SOCKADDR_IN*() const
+	{
+		return (sin_family == AF_INET ? reinterpret_cast<const SOCKADDR_IN*>(this) : 0);
+	}
+	
+	operator SOCKADDR_IN6*()
+	{
+		return (sin_family == AF_INET6 ? reinterpret_cast<SOCKADDR_IN6*>(this) : 0);
+	}
+	operator const SOCKADDR_IN6*() const
+	{
+		return (sin_family == AF_INET6 ? reinterpret_cast<const SOCKADDR_IN6*>(this) : 0);
+	}
+	
+	// 代入演算子によるSOCKADDR構造体のコピー
+	const SOCKADDR_IN& operator =(const SOCKADDR_IN& addr)
+	{
+		::CopyMemory(this, &addr, sizeof(addr));
+		
+		return *(reinterpret_cast<SOCKADDR_IN*>(this));
+	}
+	const SOCKADDR_IN6& operator =(const SOCKADDR_IN6& addr)
+	{
+		::CopyMemory(this, &addr, sizeof(addr));
+		
+		return *(reinterpret_cast<SOCKADDR_IN6*>(this));
+	}
+};
+
+// ソケットAPIのラッパークラス
+template <bool t_bManaged = true>	// t_bManaged: デストラクタによるソケットハンドル解放の有無
+class CSocketBaseT
+{
+protected:
+	SOCKET m_hSocket;	// ソケットハンドル
+
+public:
+	CSocketBaseT(SOCKET hSocket = INVALID_SOCKET)
+	{
+		m_hSocket = hSocket;
+	}
+	CSocketBaseT(int af, int type = SOCK_STREAM, int protocol = 0)
+	{
+		Socket(af, type, protocol);
+	}
+	virtual ~CSocketBaseT()
+	{
+		if (t_bManaged)
+			Close();
+	}
+	operator SOCKET() const	// キャスト演算子によるソケットハンドル取得
+	{
+		return m_hSocket;
+	}
+	
+	BOOL IsValid()	// ソケット有効:TRUE
+	{
+		return m_hSocket != INVALID_SOCKET;
+	}
+	
+	BOOL Socket(int af = AF_INET, int type = SOCK_STREAM, int protocol = 0)
+	{
+		return m_hSocket == INVALID_SOCKET ?
+			((m_hSocket = socket(af, type, protocol)) != INVALID_SOCKET) : FALSE;
+	}
+	BOOL Bind(const CSockAddrIn& addr)
+	{
+		return SOCKET_ERROR != bind(m_hSocket, addr, sizeof(addr));
+	}
+	
+	BOOL Listen(int nConnectionBacklog = SOMAXCONN)
+	{
+		return SOCKET_ERROR != listen(m_hSocket, nConnectionBacklog);
+	}
+	BOOL Connect(const CSockAddrIn& addr)
+	{
+		return SOCKET_ERROR != connect(m_hSocket, addr, sizeof(addr));
+	}
+	SOCKET Accept(CSockAddrIn& addr)
+	{
+		int iAddrLen = sizeof(addr);
+		return accept(m_hSocket, addr, &iAddrLen);
+	}
+	BOOL Close()
+	{
+		SOCKET hSocket = m_hSocket;
+		m_hSocket = INVALID_SOCKET;	// 解放時はハンドルを初期値に戻す
+		return hSocket != INVALID_SOCKET ? (SOCKET_ERROR != closesocket(hSocket)) : FALSE;
+	}
+	BOOL Shutdown(int iHow = SD_SEND)
+	{
+		return SOCKET_ERROR != shutdown(m_hSocket, iHow);
+	}
+	
+	int Send(LPCVOID pBuf, int iLen)
+	{
+		return send(m_hSocket, static_cast<const char*>(pBuf), iLen, 0);
+	}
+	int Receive(LPVOID pBuf, int iLen)
+	{
+		return recv(m_hSocket, static_cast<char*>(pBuf), iLen, 0);
+	}
+	
+	int SendTo(LPCVOID pBuf, int iLen, const CSockAddrIn& addr)
+	{
+		return sendto(m_hSocket, static_cast<const char*>(pBuf), iLen, 0, addr, sizeof(addr));
+	}
+	int RecvFrom(LPVOID pBuf, int iLen, CSockAddrIn& addr)
+	{
+		int iFromLen = sizeof(addr);
+		return recvfrom(m_hSocket, static_cast<char*>(pBuf), iLen, 0, addr, &iFromLen);
+	}
+	
+	BOOL GetPeerName(CSockAddrIn& addr)
+	{
+		int iNameLen = sizeof(addr);
+		return SOCKET_ERROR != getpeername(m_hSocket, addr, &iNameLen);
+	}
+	BOOL GetSockName(CSockAddrIn& addr)
+	{
+		int iNameLen = sizeof(addr);
+		return SOCKET_ERROR != getsockname(m_hSocket, addr, &iNameLen);
+	}
+	
+	BOOL SetSockOpt(int optname, const char* optval, int optlen)
+	{
+		return SOCKET_ERROR != setsockopt(m_hSocket, SOL_SOCKET, optname, optval, optlen);
+	}
+	BOOL GetSockOpt(int optname, char* optval, int* optlen)
+	{
+		return SOCKET_ERROR != getsockopt(m_hSocket, SOL_SOCKET, optname, optval, optlen);
+	}
+	
+	BOOL Ioctl(long cmd, u_long* argp)
+	{
+		return (SOCKET_ERROR != ioctlsocket(m_hSocket, cmd, argp));
+	}
+	int GetLastError()
+	{
+		return WSAGetLastError();
+	}
+};
+
+typedef CSocketBaseT<false> CSocketHandle;
+typedef CSocketBaseT<true> CSocketBase;
