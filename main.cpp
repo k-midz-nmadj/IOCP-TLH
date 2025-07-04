83af13ecdb2d6724e6ff36d0a4383e4798e257f7
k-midz-nmadj <k_midz_nmadj@hotmail.com>
2025-07-04 19:29:27 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+// main.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "IocpServer.h"
+
+#define PORT_NUM 8090
+
+// IOCPソケット実装クラス(Echoサーバ)
+class CIocpSocketSvr : public CIocpSocket
+{
+	int OnRead(DWORD dwBytes)	// 受信完了イベント
+	{
+		CHAR cBuff[64] = "";
+		int iRecSize = Receive(cBuff, sizeof(cBuff));	// データ受信
+		
+		return (iRecSize > 0 ? Write(cBuff, iRecSize) : 0);	// 受信データ返送
+	}
+};
+
+// IOCPソケット実装クラス(クライアント)
+class CIocpSocketClt : public CIocpSocket
+{
+	BOOL OnConnect(DWORD dwError)	// 接続完了イベント
+	{
+		if (dwError)	// 接続エラー
+		{
+			printf("Connect Error(NTSTATUS): 0x%08X\n", dwError);
+			return m_pThread->GetIocp()->Stop();	// 終了
+		}
+		
+		CHAR cBuff[18] = "";
+		printf("\nInput send text[16]: ");
+		if (scanf("%17[^\n]", cBuff) != 1)	// 送信テキスト入力待ち
+			return m_pThread->GetIocp()->Stop();	// 空入力でサーバ停止
+		
+		if (cBuff[16])
+		{
+			scanf("%*[^\n]");	// 超過入力カット
+			cBuff[16] = 0;
+		}
+		scanf("%*c");	// 改行スキップ
+		
+		if (Write(cBuff, lstrlenA(cBuff)) == SOCKET_ERROR)	// 入力テキスト送信
+		{
+			printf("Send Error: %d\n", WSAGetLastError());
+			m_pThread->GetIocp()->Stop();	// エラー時は終了
+		}
+		return TRUE;
+	}
+	
+	int OnRead(DWORD dwBytes)	// 受信完了イベント
+	{
+		CHAR cBuff[64] = "";
+		int iRecSize = Receive(cBuff, sizeof(cBuff));	// データ受信
+		if (iRecSize > 0)	// 受信データあり
+		{
+			printf("Receive text: %.*s\n", iRecSize, cBuff);
+			OnConnect(0);	// 送信テキスト入力
+		}
+		else
+		{
+			printf("\nDisconnected...");
+			m_pThread->GetIocp()->Stop();	// 切断時は終了
+		}
+		return iRecSize;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	WSA_STARTUP	// Winsock初期化
+	CIocpServer svr;
+	
+	if (argc == 1 && !svr.AddListener<CIocpSocketSvr>(PORT_NUM))	// ポートオープン
+		return -1;
+	
+	if (!svr.AddConnection<CIocpSocketClt>(	// リモート接続(パラメータ省略時はローカル接続)
+				argc > 1 ? argv[1] : "127.0.0.1", 
+				argc > 2 ? atoi(argv[2]) : PORT_NUM))
+		return -1;
+	
+	return svr.Start();	// サーバ起動
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
